ca9577ef67b046368b3cf73b0112723843cd310c
App Academy Student <student@appacademy.io>
2015-07-30 15:51:14 -0700

    adding delay timer on slider

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -7,6 +7,7 @@
     this.$el.on("click", "a.slide-right", this.slideRight.bind(this));
     var $firstChild = $(this.$items.children()[0]);
     $firstChild.addClass('active');
+    this.transitioning = false;
   };
 
   $.fn.carousel = function () {
@@ -16,6 +17,11 @@
   };
 
   $.Carousel.prototype.slide = function(dir) {
+    if (this.transitioning === true) {
+      return;
+    }
+
+    this.transitioning = true;
     var $oldImage = $(this.$items.children()[this.activeIdx]);
     if (dir === -1 && this.activeIdx === 0) {
       this.activeIdx = this.$items.children().length-1;
@@ -28,18 +34,23 @@
     }
     var $newImage = $(this.$items.children()[this.activeIdx]);
 
-    $oldImage.removeClass();
 
     if (dir === 1) {
+      $oldImage.addClass('left');
       $newImage.addClass('active right');
     } else {
+      $oldImage.addClass('right');
       $newImage.addClass('active left');
     }
+
+    var that = this;
+
     setTimeout(function () {
       $newImage.removeClass('right left');
     }, 0);
-    $newImage.one("transitionend", function () {
-
+    $oldImage.one("transitionend", function () {
+      $oldImage.removeClass();
+      that.transitioning = false;
     });
 
   };
